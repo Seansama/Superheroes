6442d19d998f752cf042fc881144b0807511c5ae
Shaun <seansama@pop-os.localdomain>
2023-03-27 13:11:14 +0300

    Power resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :hero, only: [:index, :show]
   resources :hero_power, only: [:create]
+  resources :power, only: [:index, :show, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
