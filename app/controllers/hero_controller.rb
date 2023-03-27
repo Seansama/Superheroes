8b3b12e4e247c4877caf836c8c5ceecbb7d1e314
Shaun <seansama@pop-os.localdomain>
2023-03-27 13:01:02 +0300

    Index and show routes for Hero controller

--- app/controllers/hero_controller.rb
+++ app/controllers/hero_controller.rb
@@ -1,2 +1,16 @@
 class HeroController < ApplicationController
+  def index
+    hero = Hero.all
+    render json: hero, status: :ok
+  end
+
+  def show
+    hero = Hero.find_by(id: params[:id])
+    if hero
+      render json: hero, serializer: HeroAndPowerSerializer
+    else
+      render json: {error: "Hero not found"}, status: :not_found
+    end
+  end
+
 end
