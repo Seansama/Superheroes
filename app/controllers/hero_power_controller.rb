5dd244056cb1595730263350c8d335837967d483
Shaun <seansama@pop-os.localdomain>
2023-03-27 13:08:42 +0300

    Create route for Hero_Power controller

--- app/controllers/hero_power_controller.rb
+++ app/controllers/hero_power_controller.rb
@@ -1,2 +1,11 @@
 class HeroPowerController < ApplicationController
+  def create
+    hero_power = HeroPower.create(attributes)
+    render json: hero_power.hero, serializer: HeroAndPowerSerializer
+  end
+
+  private
+  def attributes
+    params.permit(:strength, :power_id, :hero_id)
+  end
 end
