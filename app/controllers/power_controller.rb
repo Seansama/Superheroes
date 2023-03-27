20d39858eca2e33fe6f9c3a7077532a0f84edf8e
Shaun <seansama@pop-os.localdomain>
2023-03-27 13:17:23 +0300

    Index, show and update routes for Power controller

--- app/controllers/power_controller.rb
+++ app/controllers/power_controller.rb
@@ -1,2 +1,34 @@
 class PowerController < ApplicationController
+  def index
+    power = Power.all
+    render json: power
+  end
+
+  def show
+    power = Power.find_by(id: params[:id])
+    if power
+      render json: power
+    else
+      power_error
+    end
+  end
+
+  def update
+    power = Power.find_by(id: params[:id])
+    if power
+      power.update(power_params)
+      render json: power
+    else
+      power_error
+    end
+  end
+
+  private
+  def power_params
+    params.permit(:name, :description)
+  end
+
+  def power_error
+    render json: {error: "Power not found"}, status: :not_found
+  end
 end
